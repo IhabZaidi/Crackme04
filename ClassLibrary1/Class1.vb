1ac453d4e692da10166fb4c3e09211e7ea6759b5
IhabZaidi <ihab.bibou@gmail.com>
2020-06-16 16:08:59 +0100

    first commit

---
+++ ClassLibrary1/Class1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Class1
+    
+End Class
+Public Module dd
+    Dim dd As Date = "01/01/2020"
+    Public Sub check()
+        Dim r As Date = Date.Today
+        If r.ToString("dd/MM/yyyy") > dd Then
+            MsgBox("Trial period has been ended")
+        End If
+    End Sub
+End Module
---
+++ Crack me/Form1.vb
@@ -0,0 +1,11 @@
+﻿Imports ClassLibrary1
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dd.check()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
